6890bf4d77ae8533855ef3bfad707be6997b8857
Naomi-Ifergan <55000614+Naomi-Ifergan@users.noreply.github.com>
2022-11-03 20:20:57 -0400

    second commit
    I re-created the user textbox using an array instead of a json file. I now have autofill and the input is displayed on the screen.

--- script.js
+++ script.js
@@ -4,32 +4,66 @@
 
 // sources:
 // background image: https://wallpaperaccess.com/futuristic-house
+//https://codingartistweb.com/2021/12/autocomplete-suggestions-on-input-field-with-javascript/
 
-
-var textfield;
-var output;
-var submit;
-
-function setup() {
-
-noCanvas();
-textfield = createInput("What would make the world a perfect place?");
-//textfield.changed(newText);
-textfield.input(newTyping);
-output = select("#output");
-submit = select("#submit");
-submit.mousePressed(newText); //this allows users to click on submit to upload their entries on screen
-}
-
-function newTyping() {
-
-  output.html(textfield.value()); 
-  //createP(textfield.value()); //for every letter typed, it will appear on screen
-
-}
-
-function newText() {
-
-  createP(textfield.value());
-
-}
+//
+//
+// let answers = [ //I will be adding more data as I go. (currently asking people for answers)
+// "no racism",
+// "no antisemitism",
+// "no violence",
+// "renewable source energy",
+// "more green spaces",
+// "gender equality",
+// "no animal abuse",
+// "vegan living",
+// "no discrimination",
+// "no war",
+// "no hate crime",
+// "sustainable living",
+// "more handicap access",
+// ];
+//
+//
+// let sortedAnswers = answers.sort();
+// let input = document.getElementById("input");
+//
+//
+// //Execute function on keyup
+// input.addEventListener("keyup", (e) => {
+//   //loop through above array
+//   //Initially remove all elements (so if user erases a letter or adds new letter then clean previous outputs)
+//   removeElements();
+//   for (let i of sortedAnswers) {
+//     //convert input to lowercase and compare with each string
+//
+//     if (
+//       i.toLowerCase().startsWith(input.value.toLowerCase()) &&
+//       input.value != ""
+//     ) {
+//       //create list element
+//       let listItem = document.createElement("li");
+//       listItem.classList.add("list-items");
+//       listItem.style.cursor = "pointer";
+//       listItem.setAttribute("onclick", "displayAnswers('" + i + "')");
+//
+//       //Display matched part in bold
+//       let word = "<b>" + i.substr(0, input.value.length) + "</b>";
+//       word += i.substr(input.value.length);
+//       //display the value in array
+//       listItem.innerHTML = word;
+//       document.querySelector(".list").appendChild(listItem);
+//     }
+//   }
+// });
+// function displayAnswers(value) {
+//   input.value = value;
+//   removeElements();
+// }
+// function removeElements() {
+//   //clear all the item
+//   let items = document.querySelectorAll(".list-items");
+//   items.forEach((item) => {
+//     item.remove();
+//   });
+// }
